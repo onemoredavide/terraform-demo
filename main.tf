66698d33a3112995d9c1bd1240c62b136400d880
onemoredavide <davideantipa@gmail.com>
2025-01-08 18:29:15 +0200

    add sample s3 bucket resource

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "davideantipa-test-tf-bucket"
+}

f87b6ad3fc2735c65ff7797629b67271adadb86c
onemoredavide <davideantipa@gmail.com>
2025-01-08 18:33:38 +0200

    add variables

--- main.tf
+++ main.tf
@@ -9,10 +9,19 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+variable "bucket_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+  default = "eu-west-1"
+}
+
 provider "aws" {
-  region  = "eu-west-1"
+  region  = var.region
 }
 
 resource "aws_s3_bucket" "demo_bucket" {
-  bucket = "davideantipa-test-tf-bucket"
+  bucket = var.bucket_name
 }
